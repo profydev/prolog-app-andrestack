2f44a68420b6aac991dedc269804e1151c188218
André Silva <andre.silva@posteo.de>
2023-11-29 22:55:16 +0100

    Refactor project list rendering and add status mapping

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -1,4 +1,3 @@
-import capitalize from "lodash/capitalize";
 import mockProjects from "../fixtures/projects.json";
 
 describe("Project List", () => {
@@ -23,23 +22,50 @@ describe("Project List", () => {
     it("renders the projects", () => {
       const languageNames = ["React", "Node.js", "Python"];
 
-      // get all project cards
-      cy.get("main")
-        .find("li")
-        .each(($el, index) => {
-          // check the color and text of the status badge
-          cy.wrap($el);
+      it("renders the correct status in project cards", () => {
+        const statusMapping = {
+          error: {
+            background: "rgba(254, 243, 242, 1)",
+            color: "rgba(180, 35, 24, 1)",
+            text: "Critical",
+          },
+          warning: {
+            background: "rgba(255, 250, 235, 1)",
+            color: "rgba(181, 71, 8, 1)",
+            text: "Warning",
+          },
+          info: {
+            background: "rgba(236, 253, 243, 1)",
+            color: "rgba(2, 122, 72, 1)",
+            text: "Stable",
+          },
+        };
 
-          // check that project data is rendered
-          cy.wrap($el).contains(mockProjects[index].name);
-          cy.wrap($el).contains(languageNames[index]);
-          cy.wrap($el).contains(mockProjects[index].numIssues);
-          cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(capitalize(mockProjects[index].status));
-          cy.wrap($el)
-            .find("a")
-            .should("have.attr", "href", "/dashboard/issues");
-        });
+        // get all project cards
+        cy.get("main")
+          .find("li")
+          .each(($el, index) => {
+            type StatusKey = "error" | "warning" | "info";
+            const status: StatusKey = mockProjects[index].status as StatusKey;
+            const { background, color, text } = statusMapping[status];
+            // check the color and text of the status badge
+            cy.wrap($el)
+              .find(".container.warning, .container.error, .container.info")
+              .should("have.css", background, color)
+              .and("contain", text);
+
+            // check that project data is rendered
+            cy.wrap($el).contains(mockProjects[index].name);
+            cy.wrap($el).contains(languageNames[index]);
+            cy.wrap($el).contains(mockProjects[index].numIssues);
+            cy.wrap($el).contains(mockProjects[index].numEvents24h);
+            cy.wrap($el).contains(mockProjects[index].status);
+            // cy.wrap($el).contains(capitalize(mockProjects[index].status));
+            cy.wrap($el)
+              .find("a")
+              .should("have.attr", "href", "/dashboard/issues");
+          });
+      });
     });
   });
 });
