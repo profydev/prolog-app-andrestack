d5e69a1a28a6c941699dd8a27e3b2b2240be33d3
André Silva <andre.silva@posteo.de>
2023-12-01 13:29:00 +0100

    Add cy assertion to badge color, text and status

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -22,22 +22,22 @@ describe("Project List", () => {
     it("renders the projects and the correct status in project cards", () => {
       const languageNames = ["React", "Node.js", "Python"];
 
-      const statusMapping = {
-        error: {
-          background: "rgba(254, 243, 242, 1)",
-          color: "rgba(180, 35, 24, 1)",
-          text: "Critical",
-        },
-        warning: {
-          background: "rgba(255, 250, 235, 1)",
-          color: "rgba(181, 71, 8, 1)",
-          text: "Warning",
-        },
-        info: {
-          background: "rgba(236, 253, 243, 1)",
-          color: "rgba(2, 122, 72, 1)",
-          text: "Stable",
-        },
+      const expectedStatus = {
+        error: "Critical",
+        warning: "Warning",
+        info: "Stable",
+      };
+
+      const expectedColor = {
+        error: "rgb(180, 35, 24)",
+        warning: "rgb(181, 71, 8)",
+        info: "rgb(2, 122, 72)",
+      };
+
+      const expectedBackground = {
+        error: "rgb(254, 243, 242)",
+        warning: "rgb(255, 250, 235)",
+        info: "rgb(236, 253, 243)",
       };
 
       // get all project cards
@@ -46,21 +46,32 @@ describe("Project List", () => {
         .each(($el, index) => {
           type StatusKey = "error" | "warning" | "info";
           const status: StatusKey = mockProjects[index].status as StatusKey;
-          const { background, color, text } = statusMapping[status];
           const projects = mockProjects[index];
-          // check the color and text of the status badge
-          cy.wrap($el)
-            .find(".status-badge")
-            .should("have.css", "background-color", background)
-            .and("have.css", "color", color)
-            .and("contain", text);
+
+          // check the color and text of the status badges
+          cy.get("[data-testid='project-card_badge']")
+            .eq(0)
+            .should("have.text", expectedStatus.error)
+            .should("have.css", "background-color", expectedBackground.error)
+            .and("have.css", "color", expectedColor.error);
+          cy.get("[data-testid='project-card_badge']")
+            .eq(1)
+            .should("have.text", expectedStatus.warning)
+            .should("have.css", "background-color", expectedBackground.warning)
+            .and("have.css", "color", expectedColor.warning);
+
+          cy.get("[data-testid='project-card_badge']")
+            .eq(2)
+            .should("have.text", expectedStatus.info)
+            .should("have.css", "background-color", expectedBackground.info)
+            .and("have.css", "color", expectedColor.info);
 
           // check that project data is rendered
           cy.wrap($el).contains(projects.name);
           cy.wrap($el).contains(languageNames[index]);
           cy.wrap($el).contains(projects.numIssues);
           cy.wrap($el).contains(projects.numEvents24h);
-          cy.wrap($el).contains(projects.status);
+          cy.wrap($el).contains(expectedStatus[status]);
           // cy.wrap($el).contains(capitalize(mockProjects[index].status));
           cy.wrap($el)
             .find("a")
