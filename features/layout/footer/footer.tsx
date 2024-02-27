22a1144b2a6191aa66e9cf53fd50c5d16ee9e9f3
André Silva <andre.silva@posteo.de>
2024-02-27 12:42:19 +1000

    Refactor footer module and add footer text

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import Image from "next/image";
+
+const footerLinks = [
+  { text: "Docs", href: "#" },
+  { text: "API", href: "#" },
+  { text: "Help", href: "#" },
+  { text: "Community", href: "#" },
+];
 
 export function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.container}></div>
+    <footer>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerText}>Version 14.5.1</div>
+        <ul className={styles.linksList}>
+          {footerLinks.map((footerLink, index) => (
+            <li key={index}>
+              <a href={footerLink.href}>{footerLink.text}</a>
+            </li>
+          ))}
+          <div className={styles.footerLogo}></div>
+        </ul>
+        <Image
+          src="icons/logo-small.svg"
+          alt="Prolog Logo"
+          width={23}
+          height={33}
+        />
+      </div>
     </footer>
   );
 }
